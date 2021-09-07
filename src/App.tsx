8966eeeb61d1e8a259731d9c93d2fbbaa75d5365
Davaanyam <davaanyam8980@gmail.com>
2021-09-07 15:48:57 +0800

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,3 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Login from './pages/login/login-page'
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
